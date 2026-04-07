66cd108ff59ad20d1ddc263f464264665bbf314e
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-04-07 16:14:46 -0300

    feat: implementa User Entity e UserResource

---
+++ src/main/java/com/devpavanello/workshopmongo/entities/User.java
@@ -0,0 +1,23 @@
+package com.devpavanello.workshopmongo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    private String id;
+    private String name;
+    private String email;
+}

9994127a6cb3008603aeb554a3218f2af8a6d1cc
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-04-28 09:20:18 -0300

    feat(posts): implementa suporte a comentários embutidos

--- src/main/java/com/devpavanello/workshopmongo/entity/Post.java
+++ src/main/java/com/devpavanello/workshopmongo/entity/Post.java
@@ -1,18 +1,21 @@
 package com.devpavanello.workshopmongo.entity;
 
 import com.devpavanello.workshopmongo.dto.AuthorDTO;
+import com.devpavanello.workshopmongo.dto.CommentDTO;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
-@Document(collection = "post")
 @NoArgsConstructor
-@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Document(collection = "post")
 public class Post implements Serializable {
 
     @EqualsAndHashCode.Include
@@ -22,4 +25,14 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments =  new ArrayList<>();
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 }
